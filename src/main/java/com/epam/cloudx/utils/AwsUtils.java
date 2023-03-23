334b8d275c163ca0efd597775510e2d1ead63727
Alexander_Kononov1@epam.com <alexander_kononov1@epam.com>
2023-03-23 19:21:03 +0300

    added isInstanceAvailableFromInternet test

--- src/main/java/com/epam/cloudx/utils/AwsUtils.java
+++ src/main/java/com/epam/cloudx/utils/AwsUtils.java
@@ -5,35 +5,46 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.Instance;
 import com.amazonaws.services.ec2.model.Reservation;
 import java.util.List;
+import jdk.jfr.RecordingState;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class AwsUtils {
+
+  public static AmazonEC2 createEc2Client(String accessKey, String secretKey) {
+    BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+    Regions region = Regions.EU_CENTRAL_1;
+    return AmazonEC2ClientBuilder.standard()
+        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+        .withRegion(region)
+        .build();
+  }
+
   @SneakyThrows
-    public static String getInstanceStateByName(String name, AmazonEC2 ec2) throws Exception {
-      // Filter reservations by name
-      List<Reservation> reservations = ec2.describeInstances().getReservations().stream()
-          .filter(s -> s.getInstances().toString().contains(name)).toList();
+  public static Instance getReservationByName(String name, AmazonEC2 ec2) {
+    // Filter reservations by name
+    List<Reservation> reservations = ec2.describeInstances().getReservations().stream()
+        .filter(s -> s.getInstances().toString().contains(name)).toList();
 
-      // Find first. If more than 1, throw exception
-      Reservation reservation;
-      if(reservations.size() == 1) {
-        reservation = reservations.get(0);
-      } else {
-        throw new Exception("Duplication of names in EC2. Create unique name");
-      }
-      return reservation.getInstances().get(0).getState().getName();
+    // Find first. If more than 1, throw exception
+    Reservation reservation;
+    if (reservations.size() == 1) {
+      reservation = reservations.get(0);
+    } else {
+      throw new Exception("Duplication of names in EC2. Create unique name");
     }
+    return reservation.getInstances().get(0);
+  }
 
-    public static AmazonEC2 createEc2Client(String accessKey, String secretKey) {
-      BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-      Regions region = Regions.EU_CENTRAL_1;
-      return AmazonEC2ClientBuilder.standard()
-          .withCredentials(new AWSStaticCredentialsProvider(credentials))
-          .withRegion(region)
-          .build();
-    }
+  public static String getInstanceStateByName(String name, AmazonEC2 ec2) {
+    return getReservationByName(name, ec2).getState().getName();
+  }
+
+  public static String getPublicIpAddressByName(String name, AmazonEC2 ec2) {
+    return getReservationByName(name, ec2).getPublicIpAddress();
+  }
 }
