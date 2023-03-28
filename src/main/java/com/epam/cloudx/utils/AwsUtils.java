f07691c3f7abfe19f0d2766875cb08249c609834
Alexander_Kononov1@epam.com <alexander_kononov1@epam.com>
2023-03-28 22:37:13 +0300

    refactoring

--- src/main/java/com/epam/cloudx/utils/AwsUtils.java
+++ src/main/java/com/epam/cloudx/utils/AwsUtils.java
@@ -5,10 +5,15 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.GroupIdentifier;
 import com.amazonaws.services.ec2.model.Instance;
 import com.amazonaws.services.ec2.model.Reservation;
 import com.epam.cloudx.Exceptions.DuplicationInstanceNameException;
 import com.epam.cloudx.Exceptions.ServiceUnavailableFromPublicException;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import java.util.List;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 
@@ -25,29 +30,29 @@ public class AwsUtils {
   }
 
   @SneakyThrows
-  public static Instance getReservationByName(String name, AmazonEC2 ec2) {
-    // Filter reservations by name
-    var reservations = ec2.describeInstances().getReservations().stream()
-        .filter(s -> s.getInstances().toString().contains(name)).toList();
+    public static Instance getInstanceByName(String name, AmazonEC2 ec2) {
+      // Filter reservations by name
+      var reservations = ec2.describeInstances().getReservations().stream()
+          .filter(s -> s.getInstances().toString().contains(name)).toList();
 
-    // Find first. If more than 1, throw exception
-    Reservation reservation;
-    if (reservations.size() == 1) {
-      reservation = reservations.get(0);
-    } else {
-      throw new DuplicationInstanceNameException("Duplication of names in EC2. Create unique name");
+      // Find first. If more than 1, throw exception
+      Reservation reservation;
+      if (reservations.size() == 1) {
+        reservation = reservations.get(0);
+      } else {
+        throw new DuplicationInstanceNameException("Duplication of names in EC2. Create unique name");
+      }
+      return reservation.getInstances().get(0);
     }
-    return reservation.getInstances().get(0);
-  }
 
   public static String getInstanceStateByName(String name, AmazonEC2 ec2) {
-    return getReservationByName(name, ec2).getState().getName();
+    return getInstanceByName(name, ec2).getState().getName();
   }
 
   @SneakyThrows
   public static String getPublicIpAddressByName(String name, AmazonEC2 ec2) {
-    if(getReservationByName(name, ec2).getPublicIpAddress() != null) {
-      return getReservationByName(name, ec2).getPublicIpAddress();
+    if(getInstanceByName(name, ec2).getPublicIpAddress() != null) {
+      return getInstanceByName(name, ec2).getPublicIpAddress();
     } else {
       throw new ServiceUnavailableFromPublicException("Public IP is empty. Instance is not accessible from internet");
     }
@@ -55,10 +60,29 @@ public class AwsUtils {
 
   @SneakyThrows
   public static String getPrivateIpAddressByName(String name, AmazonEC2 ec2) {
-    if(getReservationByName(name, ec2).getPublicIpAddress() != null) {
-      return getReservationByName(name, ec2).getPrivateIpAddress();
+    if(getInstanceByName(name, ec2).getPublicIpAddress() != null) {
+      return getInstanceByName(name, ec2).getPrivateIpAddress();
     } else {
       throw new ServiceUnavailableFromPublicException("Private IP is empty. Instance is not accessible from internet");
+    }
+  }
+  @SneakyThrows
+  public static boolean isSshAccessible(Instance instance, String keyPairFilePath) {
+    String publicIpAddress = instance.getPublicIpAddress();
+    if (publicIpAddress == null) {
+      return false; // instance does not have a public IP address
+    }
+
+    JSch jsch = new JSch();
+    try {
+      jsch.addIdentity(keyPairFilePath);
+      Session session = jsch.getSession("ec2-user", publicIpAddress, 22);
+      session.setConfig("StrictHostKeyChecking", "no"); // disable host key checking
+      session.connect(1000); // connect with a timeout of 1 second
+      session.disconnect();
+      return true;
+    } catch (JSchException e) {
+      return false; // SSH connection failed
     }
   }
 }
