8cfdeaacba278abb673bc8cd0ed9666e5e4d66ab
Alexander_Kononov1@epam.com <alexander_kononov1@epam.com>
2023-03-28 22:37:13 +0300

    refactoring

--- src/main/java/com/epam/cloudx/tests/deploymentValidation/GetEc2InstancesPublicTest.java
+++ src/main/java/com/epam/cloudx/tests/deploymentValidation/GetEc2InstancesPublicTest.java
@@ -10,27 +10,28 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GetEc2InstancesPublicTest extends PublicInstanceTest {
-    @Test
-    public void isInstanceRunning() {
-        final String expectedState = "running";
-        final String actualState = AwsUtils.getInstanceStateByName(publicInstanceName, ec2);
 
-        Assertions.assertEquals(actualState, expectedState,
-            String.format("Actual instance state is %s.", actualState));
-    }
+  @Test
+  public void isInstanceRunning() {
+    final String expectedState = "running";
+    final String actualState = AwsUtils.getInstanceStateByName(publicInstanceName, ec2);
 
-    @Test
-    public void publicInstanceIsAvailableFromInternet() {
-        boolean isPublicIpExisting = !AwsUtils.getPublicIpAddressByName(publicInstanceName, ec2).isEmpty();
-        Assertions.assertTrue(isPublicIpExisting, "Public IP is empty");
-    }
+    Assertions.assertEquals(actualState, expectedState,
+        String.format("Actual instance state is %s.", actualState));
+  }
 
-    @Test
-    public void getApiForPublicInstance() {
-        var file = new File("src/main/resources/data/appInfoPublic.json");
-        var response = HttpUtils.getPublicAppInfo(publicInstanceName, ec2);
-        var actualResponse = JsonUtils.readJsonAsObject(response, AppInfo.class);
-        var expectedResponse = JsonUtils.readJsonFileAsObject(file, AppInfo.class);
-        Assertions.assertEquals(actualResponse, expectedResponse);
-    }
+  @Test
+  public void publicInstanceIsAvailableFromInternet() {
+    boolean isPublicIpExisting = !AwsUtils.getPublicIpAddressByName(publicInstanceName, ec2).isEmpty();
+    Assertions.assertTrue(isPublicIpExisting, "Public IP is empty");
+  }
+
+  @Test
+  public void getApiForPublicInstance() {
+    var file = new File("src/main/resources/data/json/appInfoPublic.json");
+    var response = HttpUtils.getPublicAppInfo(publicInstanceName, ec2);
+    var actualResponse = JsonUtils.readJsonAsObject(response, AppInfo.class);
+    var expectedResponse = JsonUtils.readJsonFileAsObject(file, AppInfo.class);
+    Assertions.assertEquals(actualResponse, expectedResponse);
+  }
 }
