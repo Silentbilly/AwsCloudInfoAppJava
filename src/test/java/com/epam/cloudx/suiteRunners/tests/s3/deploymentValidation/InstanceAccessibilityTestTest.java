18736a3526b3bc13099830654b9b1129ddd540bd
Alexander_Kononov1@epam.com <alexander_kononov1@epam.com>
2023-04-13 02:53:17 +0300

    add s3 deployment validation tests

---
+++ src/test/java/com/epam/cloudx/suiteRunners/tests/s3/deploymentValidation/InstanceAccessibilityTestTest.java
@@ -0,0 +1,62 @@
+package com.epam.cloudx.suiteRunners.tests.s3.deploymentValidation;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.epam.cloudx.suiteRunners.tests.CloudxImageBaseTest;
+import com.epam.cloudx.utils.AwsUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The application is deployed in the public subnet and should be accessible by HTTP from the internet via an Internet
+ * gateway by public IP address and FQDN.
+ * The application instance should be accessible by SSH protocol.
+ * The application should have access to the S3 bucket via an IAM role.
+ */
+
+class InstanceAccessibilityTestTest extends CloudxImageBaseTest {
+
+  @Test
+  @DisplayName("The application is deployed in the public subnet")
+  @Tag("s3")
+  @Tag("smoke")
+  void subnetIsPublic() {
+    final boolean isSubnetPublic = AwsUtils.isAppInPublicSubnet(ec2, publicInstanceName);
+    Assertions.assertTrue(isSubnetPublic, "The app must be in public subnet");
+  }
+
+  @Test
+  @DisplayName("The application is accessible by HTTP from the internet")
+  @Tag("s3")
+  @Tag("smoke")
+  void instanceAccessibleByPublicIpAddress() {
+    final boolean isAccessibleByPublicIp = AwsUtils.isInstanceAccessibleByPublicIpAddress(ec2, publicInstanceName);
+    final boolean isAccessibleByHttp = AwsUtils.isInstanceAccessibleByHttp(ec2, publicInstanceName);
+    assertAll(
+        "accessibility by HHTP and public ip",
+        () -> assertTrue(isAccessibleByPublicIp, "The app must be accessible by public IP"),
+        () -> assertTrue(isAccessibleByHttp, "The app must be accessible by public HTTP")
+    );
+  }
+
+  @Test
+  @DisplayName("The application is accessible by FQDN from the internet")
+  @Tag("s3")
+  @Tag("smoke")
+  void instanceAccessibleByFqdn() {
+    final boolean isAccessible = AwsUtils.isInstanceAccessibleByFqdn(ec2, publicInstanceName);
+    Assertions.assertTrue(isAccessible, "The app must be accessible by FQDN");
+  }
+
+  @Test
+  @DisplayName("The application instance should be accessible by SSH protocol")
+  @Tag("s3")
+  @Tag("smoke")
+  void appAccessibleBySsh() {
+    final boolean isAccessible = AwsUtils.isInstanceAccessibleBySsh(ec2, publicInstanceName);
+    Assertions.assertTrue(isAccessible, "The app must be accessible by SSH");
+  }
+}
