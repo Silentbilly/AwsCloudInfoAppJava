7178c2845ec6f7de5fb2ebf0b550e9cf365d7076
Alexander_Kononov1@epam.com <alexander_kononov1@epam.com>
2023-04-13 02:55:50 +0300

    redact paths to suites

--- src/test/java/com/epam/cloudx/suiteRunners/Ec2TestRunner.java
+++ src/test/java/com/epam/cloudx/suiteRunners/Ec2TestRunner.java
@@ -1,7 +1,7 @@
 package com.epam.cloudx.suiteRunners;
 import org.junit.platform.suite.api.*;
 
-@SelectPackages({"com.epam.cloudx.tests.ec2"})
+@SelectPackages({"com.epam.cloudx.suiteRunners.tests.ec2"})
 @IncludeTags({"public", "private", "configuration"})
 @Suite
 public class Ec2TestRunner {
