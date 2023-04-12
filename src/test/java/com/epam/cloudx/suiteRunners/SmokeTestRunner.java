e4fb7f4300de78c28f3f0f14f5fb032dc49d4cc4
Alexander_Kononov1@epam.com <alexander_kononov1@epam.com>
2023-04-13 02:55:50 +0300

    redact paths to suites

--- src/test/java/com/epam/cloudx/suiteRunners/SmokeTestRunner.java
+++ src/test/java/com/epam/cloudx/suiteRunners/SmokeTestRunner.java
@@ -4,7 +4,7 @@ import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
-@SelectPackages({"com.epam.cloudx.tests"})
+@SelectPackages({"com.epam.cloudx.suiteRunners.tests"})
 @IncludeTags({"smoke", "s3"})
 @Suite
 public class SmokeTestRunner {
--- src/test/java/com/epam/cloudx/suiteRunners/VpcTestRunner.java
+++ src/test/java/com/epam/cloudx/suiteRunners/VpcTestRunner.java
@@ -4,7 +4,7 @@ import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
-@SelectPackages({"com.epam.cloudx.tests.vpc"})
+@SelectPackages({"com.epam.cloudx.suiteRunners.tests.vpc"})
 @IncludeTags({"vpc"})
 @Suite
 public class VpcTestRunner {
