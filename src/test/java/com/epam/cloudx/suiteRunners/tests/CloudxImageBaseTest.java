fcb981093a05c70c3018369843d76395cb19e5b1
Alexander_Kononov1@epam.com <alexander_kononov1@epam.com>
2023-06-13 01:33:12 +0300

    create deployment validation tests for sns-sqs

--- src/test/java/com/epam/cloudx/suiteRunners/tests/CloudxImageBaseTest.java
+++ src/test/java/com/epam/cloudx/suiteRunners/tests/CloudxImageBaseTest.java
@@ -2,23 +2,37 @@ package com.epam.cloudx.suiteRunners.tests;
 import com.epam.cloudx.config.Config;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.rds.RdsClient;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sqs.SqsClient;
 
 public abstract class CloudxImageBaseTest extends BaseTest {
-  private Config INSTANCE = Config.getInstance();
+  private final Config INSTANCE = Config.getInstance();
+  AwsBasicCredentials credentials = AwsBasicCredentials.create(INSTANCE.getAccessKey(), INSTANCE.getSecretKey());
 
   protected String publicInstanceName = "cloudximage/AppInstance/Instance";
   protected String filePath = "src/main/resources/data/files/soap_vs_rest.png";
   protected String dbInstanceName = "cloudximage-databasemysqlinstance";
 
   protected S3Client s3Client = S3Client.builder()
-      .credentialsProvider(DefaultCredentialsProvider.create())
+      .credentialsProvider(StaticCredentialsProvider.create(credentials))
       .region(Region.EU_CENTRAL_1)
       .build();
 
-  AwsBasicCredentials credentials = AwsBasicCredentials.create(INSTANCE.getAccessKey(), INSTANCE.getSecretKey());
+  protected SnsClient snsClient = SnsClient.builder()
+      .region(Region.EU_CENTRAL_1)
+      .credentialsProvider(StaticCredentialsProvider.create(credentials))
+      .build();
+
+  protected SqsClient sqsClient = SqsClient.builder()
+      .region(Region.EU_CENTRAL_1)
+      .credentialsProvider(StaticCredentialsProvider.create(credentials))
+      .build();
+
   protected RdsClient rdsClient = RdsClient.builder()
       .credentialsProvider(() -> credentials)
       .region(Region.EU_CENTRAL_1)
